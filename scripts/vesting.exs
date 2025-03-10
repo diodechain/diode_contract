c5ac16853e197120188a74dbea75464dfcc23798
Dominic Letz <dominic@diode.io>
2025-03-10 16:12:27 +0100

    Added vesting script

--- scripts/vesting.exs
+++ scripts/vesting.exs
@@ -7,26 +7,23 @@ defmodule VestingContract do
     nil
   end
 
-  def parse(ref, amount, revocable, destination, contract) do
-    data =
-      String.split(String.trim(contract), ";", trim: true)
-      |> Enum.map(fn field ->
-        [key, value] = String.split(field, ":", trim: true)
-        {String.trim(String.downcase(key)), String.trim(value)}
-      end)
-      |> Enum.into(%{})
+  def parse(ref, amount, revocable, destination, row) do
+    start = to_linux_timestamp(row["start"])
 
-    start = to_epoch(data["start"])
-    vesting_end = to_epoch(data["end"])
-    cliff_end = to_epoch(data["cliff"])
+    # TODO: Lock is already encoded in the start date
+    # start = case row["lock"] do
+    #   "None" -> start
+    #   months ->
+    #     if String.ends_with?(months, "mo") do
+    #       months = String.trim_trailing(months, "mo") |> String.to_integer()
+    #       start + months * 30 * 24 * 60 * 60
+    #     else
+    #       raise "Invalid lock duration: #{row["lock"]}"
+    #     end
+    # end
 
-    amount =
-      if String.ends_with?(data["amount"], "%") do
-        percentage = String.trim(data["amount"], "%") |> String.to_integer()
-        trunc(amount * (percentage / 100))
-      else
-        data["amount"] |> String.to_integer()
-      end
+    vesting_end = to_linux_timestamp(row["end"])
+    cliff_end = to_linux_timestamp(row["cliff"]) || start
 
     cliff_duration = cliff_end - start
     duration = vesting_end - start
@@ -48,7 +45,8 @@ defmodule VestingContract do
     }
   end
 
-  def to_epoch(date) do
+  def to_linux_timestamp("None"), do: nil
+  def to_linux_timestamp(date) do
     DateTime.new!(
       Date.from_iso8601!(String.replace(date, "/", "-")),
       Time.from_iso8601!("00:00:00")
@@ -74,21 +72,19 @@ defmodule VestingContract do
 end
 
 separator = "\t"
-[fields, vestings] = File.read!("vestings.txt")
+[fields | vestings] = File.read!("vestings.txt")
   |> String.split("\n", trim: true)
 
 fields = String.split(fields, separator, trim: true) |> Enum.map(&String.downcase/1)
 
 for row <- vestings do
-  row = List.zip([fields, String.split(row, separator, trim: true)]) |> Map.new()
+  row = Enum.zip([fields, String.split(row, separator, trim: true)]) |> Map.new()
 
   ref = String.to_integer(row["ref"])
   amount = String.to_integer(String.replace(row["amount"], ",", ""))
-  revocable = row["revocable"] == "Revocable"
-
-  IO.puts("\nref: #{ref} amount: #{amount} revocable: #{revocable} destination: #{destination}")
-  vesting1 = VestingContract.parse(ref, amount, revocable, destination)
+  revocable = row["revocable"]
+  destination = row["destination"]
+  IO.puts("\n# ref: #{ref} amount: #{amount} revocable: #{revocable} destination: #{destination}")
+  vesting1 = VestingContract.parse(ref, amount, revocable, destination, row)
   IO.puts(VestingContract.cmd(vesting1))
-  vesting2 = VestingContract.parse(ref, amount, revocable, destination)
-  IO.puts(VestingContract.cmd(vesting2))
 end
