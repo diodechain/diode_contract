602a1e0dca1f7d7ca8e7ced0aac7618603a7856e
Dominic Letz <dominic@diode.io>
2025-03-19 18:58:36 +0100

    initial oasis deployment

---
+++ ZTNALayout.md
@@ -0,0 +1,8 @@
+# ZTNA Layout
+
+## Oasis Sapphire Testnet
+
+Version: 100    0x988C891e92678e06bf0e7F01c9327208EE294a5c
+Proxy:          0x18D1c56474505893082e1B50A7c5a7cdc7854Eca
+
+
--- contracts/IProxyResolver.sol
+++ contracts/IProxyResolver.sol
@@ -2,7 +2,7 @@
 // Diode Contracts
 // Copyright 2021-2024 Diode
 // Licensed under the Diode License, Version 1.0
-pragma solidity ^0.7.6;
+pragma solidity >=0.7.6;
 
 interface IProxyResolver {
     function resolve(bytes32 ref) external view returns (address);
