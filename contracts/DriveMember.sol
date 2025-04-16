36876989765c0bc51b8d009bca6dfbc3c51cad0c
Dominic Letz <dominic@diode.io>
2025-04-16 17:31:24 +0200

    Added MetaTransactions to Identity contract

--- contracts/DriveMember.sol
+++ contracts/DriveMember.sol
@@ -9,14 +9,14 @@ import "./Group.sol";
 
 /**
  * DriveMember and Identity Smart Contract
- *  
+ *
  * When used as a DriveMember the `drive` member variable should point to the address of the drive "zone"
  * that this contract is a member of.
  *
  * When used as Identity the `drive` member variable will be `0`.
  *
  * The "owner" will always be the initial "master key". The contract is being deployed using the DriveFactory.sol
- * and thus upgradeable by the `owner()` only. 
+ * and thus upgradeable by the `owner()` only.
  *
  * "additional_drives" is temporary to store multiple Zones all used with this same identity. Will be replaced
  * once the client can handle multiple identity connections.
@@ -30,15 +30,28 @@ contract DriveMember is Group {
     bool protected;
     address drive;
     address[] additional_drives;
+    mapping(uint256 => bool) public nonces;
 
-    modifier onlyMember override {
-        if (protected) {
-            require(owner() == msg.sender, "Only the owner can call this in protected mode");
-        } else {
-            require(owner() == msg.sender || members.IsMember(msg.sender), "Only members can call this");
-        }
-
+    modifier onlyMember() override {
+        requireMember(msg.sender);
         _;
+    }
+
+    function requireMember(address _member) internal view {
+        // MetaTransaction calls AddDrive/SetDrive on itself
+        if (_member == address(this)) return;
+
+        if (protected) {
+            require(
+                owner() == _member,
+                "Only the owner can call this in protected mode"
+            );
+        } else {
+            require(
+                owner() == _member || members.IsMember(_member),
+                "Only members can call this"
+            );
+        }
     }
 
     constructor() {
@@ -46,8 +59,8 @@ contract DriveMember is Group {
         update_change_tracker();
     }
 
-    function Version() external virtual pure returns (int256) {
-        return 114;
+    function Version() external pure virtual returns (int256) {
+        return 115;
     }
 
     function Protect(bool _protect) external onlyMember {
@@ -90,13 +103,100 @@ contract DriveMember is Group {
         return additional_drives;
     }
 
-    function SubmitTransaction(address dst, bytes memory data) public onlyMember
-    {
-        require(external_call(dst, data.length, data), "General Transaction failed");
+    function SubmitTransaction(
+        address dst,
+        bytes memory data
+    ) public onlyMember {
+        require(
+            external_call(dst, data.length, data),
+            "General Transaction failed"
+        );
     }
 
-    function SubmitDriveTransaction(bytes memory data) public onlyMember
-    {
-        require(external_call(drive, data.length, data), "Drive Transaction failed");
+    function SubmitDriveTransaction(bytes memory data) public onlyMember {
+        require(
+            external_call(drive, data.length, data),
+            "Drive Transaction failed"
+        );
+    }
+
+    function DOMAIN_SEPARATOR() public view returns (bytes32) {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+
+        return
+            keccak256(
+                abi.encode(
+                    // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
+                    0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
+                    keccak256("DriveMember"),
+                    keccak256("115"),
+                    chainId,
+                    address(this)
+                )
+            );
+    }
+
+    function toTypedDataHash(
+        bytes32 domainSeparator,
+        bytes32 structHash
+    ) internal pure returns (bytes32 data) {
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, "\x19\x01")
+            mstore(add(ptr, 0x02), domainSeparator)
+            mstore(add(ptr, 0x22), structHash)
+            data := keccak256(ptr, 0x42)
+        }
+    }
+
+    function TransactionDigest(
+        uint256 nonce,
+        uint256 deadline,
+        address dst,
+        bytes memory data
+    ) public view returns (bytes32) {
+        return
+            toTypedDataHash(
+                DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "Transaction(uint256 nonce,uint256 deadline,address dst,bytes data)"
+                        ),
+                        nonce,
+                        deadline,
+                        dst,
+                        keccak256(data)
+                    )
+                )
+            );
+    }
+
+    function SubmitMetaTransaction(
+        uint256 nonce,
+        uint256 deadline,
+        address dst,
+        bytes memory data,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        require(block.timestamp < deadline, "Transaction expired");
+        require(nonces[nonce] == false, "Invalid nonce");
+        nonces[nonce] = true;
+        bytes32 digest = TransactionDigest(nonce, deadline, dst, data);
+        address signer = ecrecover(digest, v, r, s);
+        require(signer != address(0), "Invalid signature");
+        require(
+            owner() == signer || members.IsMember(signer),
+            "Invalid signer"
+        );
+        require(
+            external_call(dst, data.length, data),
+            "General Transaction failed"
+        );
     }
 }
