a8c74b8b6d97ee83d19e1c6f978cc1666f3a40ed
Dominic Letz <dominic@diode.io>
2025-07-24 15:43:42 +0200

    DiodeNodeRegistry v1

---
+++ contracts/DiodeNodeRegistry.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: DIODE
+// Diode Contracts
+// Copyright 2021-2025 Diode
+// Licensed under the Diode License, Version 1.0
+pragma solidity ^0.8.20;
+pragma experimental ABIEncoderV2;
+
+import "./deps/IERC20.sol";
+import "./deps/SafeERC20.sol";
+import "./deps/Utils.sol";
+import "./deps/SafeMath.sol";
+import "./deps/Initializable.sol";
+import "./deps/Set.sol";
+
+/**
+ * DiodeNodeRegistry
+ *
+ * Registry for Diode Nodes which stake tokens.
+ *
+ */
+contract DiodeNodeRegistry is Initializable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    using Set for Set.Data;
+
+    address public immutable Foundation;
+    IERC20 public immutable Token;
+    uint256 public minimumStake;
+
+    struct Node {
+        address accountant;
+        address nodeAddress;
+        uint256 stake;
+    }
+
+    mapping(address => Node) public nodes;
+    Set.Data nodesSet;
+
+    constructor(address _foundation, address _token) initializer {
+        Foundation = _foundation;
+        Token = IERC20(_token);
+    }
+
+    function initialize() public initializer {
+        require(msg.sender == Foundation, "Only the foundation can initialize");
+        minimumStake = 0 ether;
+    }
+
+    function registerNode(address _nodeAddress, address _accountant, uint256 _stake) public {
+        require(msg.sender == _nodeAddress, "Only the node itself can register");
+
+        Node storage node = nodes[_nodeAddress];
+        require(_stake + node.stake > minimumStake, "Node must have at least minimum stake");
+
+        if (_stake > 0) {
+            Token.safeTransferFrom(msg.sender, address(this), _stake);
+            node.stake = _stake;
+        }
+
+        node.accountant = _accountant;
+        node.nodeAddress = _nodeAddress;
+        nodesSet.Add(_nodeAddress);
+    }
+
+    function unstakeNode(address _nodeAddress) public {
+        require(msg.sender == _nodeAddress || msg.sender == nodes[_nodeAddress].accountant, "Only the node itself or its accountant can unstake");
+
+        Node storage node = nodes[_nodeAddress];
+        require(node.stake > 0, "Node must have a stake");
+        require(node.accountant != address(0), "Node must have an accountant");
+
+        Token.safeTransfer(node.accountant, node.stake);
+        node.stake = 0;
+        node.accountant = address(0);
+        node.nodeAddress = address(0);
+        nodesSet.Remove(_nodeAddress);
+    }
+
+    function getNodes() public view returns (address[] memory) {
+        return nodesSet.Members();
+    }
+
+    function version() public pure returns (uint256) {
+        return 100;
+    }
+}
--- scripts/deploy_moonbeam.sh
+++ scripts/deploy_moonbeam.sh
@@ -4,12 +4,13 @@
 # ./scripts/deploy_moonbeam.sh contracts/BNS.sol:BNS
 # ./scripts/deploy_moonbeam.sh contracts/DiodeToken.sol:DiodeToken --constructor-args 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704 0xA32a9eD71fBF22e6D197c13725Ad61958e9a4499 false
 # ./scripts/deploy_moonbeam.sh contracts/DiodeRegistryLight.sol:DiodeRegistryLight --constructor-args 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704 0x434116a99619f2B465A137199C38c1Aab0353913
-# ./scripts/deploy_moonbeam.sh contracts/Proxy.sol:Proxy --constructor-args 0x75637505b914eC9C6e9B8eDe383605cD117b0C99 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704
+# ./scripts/deploy_moonbeam.sh contracts/Proxy8.sol:Proxy8 --constructor-args 0x75637505b914eC9C6e9B8eDe383605cD117b0C99 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704
 # ./scripts/deploy_moonbeam.sh contracts/DevFleetContract.sol:DevFleetContract --constructor-args 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704
 # ./scripts/deploy_moonbeam.sh contracts/DriveMember.sol:DriveMember
 # ./scripts/deploy_moonbeam.sh contracts/Drive.sol:Drive --constructor-args 0x8a093e3A83F63A00FFFC4729aa55482845a49294
 # ./scripts/deploy_moonbeam.sh contracts/FleetContractUpgradeable.sol:FleetContractUpgradeable --constructor-args 0xD78653669fd3df4dF8F3141Ffa53462121d117a4
 # ./scripts/deploy_moonbeam.sh contracts/Bridge.sol:Bridge --constructor-args 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704 [] 3
+# ./scripts/deploy_moonbeam.sh contracts/DiodeNodeRegistry.sol:DiodeNodeRegistry --constructor-args 0x3d565Ec28595c1a0710ABCBd8C0F979d31E38704 0x434116a99619f2B465A137199C38c1Aab0353913
 
 
 # BERRY TEST: ./scripts/deploy_moonbeam.sh contracts/YieldVault.sol:YieldVault --constructor-args 0xb9f1fa217c9a86915b4368e4ee9f079d6e5644a9 31536000 31536000 1500
