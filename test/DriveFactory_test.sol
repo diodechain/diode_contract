fbc85446d7b1a46eb9d7b9897c24714ec23e88ad
Dominic Letz <dominic@diode.io>
2025-10-30 13:40:27 +0100

    minor

--- test/DriveFactory_test.sol
+++ test/DriveFactory_test.sol
@@ -48,7 +48,7 @@ contract DriveFactoryTest {
         drive.AddMember(number1, RoleType.Admin);
 
         // Factory created contract should work normally
-        Assert.equal(drive.Version(), 143, "Version() should be equal 143");
+        Assert.equal(drive.Version(), 144, "Version() should be equal 144");
         acceptanceTest(drive);
 
         // Upgrade
