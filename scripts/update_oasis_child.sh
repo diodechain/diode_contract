93a864b66a667618251ecff45ee4338d41e980c1
Dominic Letz <dominic@diode.io>
2026-03-02 14:27:44 +0100

    Added script to update oasis child contracts

---
+++ scripts/update_oasis_child.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Examples:
+# ./scripts/update_oasis.sh 0xa51fBD85e1e4e1AC5ecd6b7DF5DB6993dA7030bb
+
+export PROXY=0xf90314E31D34C7ad82382f1a9dCB5Fc0FDA71ACe
+export RPC=https://sapphire.oasis.io
+
+# export PROXY=0x18D1c56474505893082e1B50A7c5a7cdc7854Eca
+# export RPC=https://testnet.sapphire.oasis.io
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+set -e
+
+
+
+TYPE=`cast call $1 "Type()" --rpc-url $RPC`
+if [ "$TYPE" == "" ]; then
+    echo "Failed to get type"
+    exit 1
+fi
+
+OLD_IMPL_ADDR=`cast call $PROXY "resolve(bytes32)" $TYPE --rpc-url $RPC | cast parse-bytes32-address`
+
+echo "Old implementation address $OLD_IMPL_ADDR"
+OLD_VSN=`cast call $OLD_IMPL_ADDR "Version()" --rpc-url $RPC | cast td`
+if [ "$OLD_VSN" == "" ]; then
+    echo "Failed to get current version"
+    exit 1
+fi
+echo "Current version $OLD_VSN"
+
+NEW_VSN=`cast call $1 "Version()" --rpc-url $RPC | cast td`
+if [ "$NEW_VSN" == "" ]; then
+    echo "Failed to get new version"
+    exit 1
+fi
+echo "New version $NEW_VSN"
+
+OLD_HASH=$(cast keccak $(cast code $OLD_IMPL_ADDR --rpc-url $RPC))
+NEW_HASH=$(cast keccak $(cast code $1 --rpc-url $RPC))
+
+if [ "$OLD_HASH" == "$NEW_HASH" ]; then
+    echo "Code is the same, skipping"
+    exit 0
+fi
+
+if [ "$OLD_VSN" == "$NEW_VSN" ]; then
+    echo "Versions are the same but code is different, did you forget to update the version?"
+    exit 1
+fi
+
+set -x
+cast send --legacy --rpc-url $RPC --private-key $(cat diode_glmr.key) $PROXY "SetContractInfo(address)" $1
